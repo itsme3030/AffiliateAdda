baf6b03584796e881cc18e52115bc7d2390bf8ab
yash-sojitra-20 <yash.sojitra.2020@gmail.com>
2024-12-23 00:48:04 +0530

    Dto created

---
+++ src/main/java/com/example/paypergo/dto/LinkDto.java
@@ -0,0 +1,34 @@
+package com.example.paypergo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+public class LinkDto {
+    private Long user_id;
+    private Long product_id;
+
+    public LinkDto(Long user_id, Long product_id) {
+        this.user_id = user_id;
+        this.product_id = product_id;
+    }
+
+    public Long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Long user_id) {
+        this.user_id = user_id;
+    }
+
+    public Long getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(Long product_id) {
+        this.product_id = product_id;
+    }
+}
--- src/main/java/com/example/paypergo/dto/UserDto.java
+++ src/main/java/com/example/paypergo/dto/UserDto.java
@@ -4,13 +4,42 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
 public class UserDto {
 
-    private String username;
-    private String email;
-    private String password;
+    private String user_username;
+    private String user_email;
+    private String user_password;
 
+    public UserDto(String user_username, String user_email, String user_password) {
+        this.user_username = user_username;
+        this.user_email = user_email;
+        this.user_password = user_password;
+    }
+
+    public String getUser_username() {
+        return user_username;
+    }
+
+    public void setUser_username(String user_username) {
+        this.user_username = user_username;
+    }
+
+    public String getUser_email() {
+        return user_email;
+    }
+
+    public void setUser_email(String user_email) {
+        this.user_email = user_email;
+    }
+
+    public String getUser_password() {
+        return user_password;
+    }
+
+    public void setUser_password(String user_password) {
+        this.user_password = user_password;
+    }
 }
