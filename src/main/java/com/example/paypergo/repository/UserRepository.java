8fbe911f9453530d3f996a557e180458ee6480a9
yash-sojitra-20 <yash.sojitra.2020@gmail.com>
2024-12-22 22:23:13 +0530

    user repository created

---
+++ src/main/java/com/example/paypergo/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.paypergo.repository;
+
+import com.example.paypergo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserEmail(String email);
+}
