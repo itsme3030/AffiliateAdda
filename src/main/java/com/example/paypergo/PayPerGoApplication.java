c8fd996208e390944044a1095d4edf0a66da0c2f
yash-sojitra-20 <yash.sojitra.2020@gmail.com>
2024-12-22 15:26:54 +0530

    initial setup

---
+++ src/main/java/com/example/paypergo/PayPerGoApplication.java
@@ -0,0 +1,13 @@
+package com.example.paypergo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayPerGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayPerGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/paypergo/PayPerGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paypergo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayPerGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
