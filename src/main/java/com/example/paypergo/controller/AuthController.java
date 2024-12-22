33b2eebfc914de899b3f6554857fa15d273699b1
yash-sojitra-20 <yash.sojitra.2020@gmail.com>
2024-12-22 22:00:20 +0530

    auth controller created

---
+++ src/main/java/com/example/paypergo/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.example.paypergo.controller;
+
+import com.example.paypergo.dto.UserDto;
+import com.example.paypergo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody UserDto userDto) {
+        try{
+            User user = authService.registerUser(userDto.getUsername(), userDto.getEmail(), userDto.getPassword());
+            return new ResponseEntity<>(user, HttpStatus.CREATED);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody UserDto userDto) {
+        try{
+            User user = authService.loginUser(userDto.getEmail(), userDto.getPassword());
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        }catch (RuntimeException e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+}
