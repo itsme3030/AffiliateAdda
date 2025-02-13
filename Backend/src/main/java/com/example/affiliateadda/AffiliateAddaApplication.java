8d130e2328b39ad5446785b65793de016a44815a
yash-sojitra-20 <yash.sojitra.2020@gmail.com>
2025-02-13 21:45:21 +0530

    PayPerGo -> AffiliateAdda

--- Backend/src/main/java/com/example/affiliateadda/AffiliateAddaApplication.java
+++ Backend/src/main/java/com/example/affiliateadda/AffiliateAddaApplication.java
@@ -1,13 +1,13 @@
-package com.example.paypergo;
+package com.example.affiliateadda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PayPerGoApplication {
+public class AffiliateAddaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PayPerGoApplication.class, args);
+		SpringApplication.run(AffiliateAddaApplication.class, args);
 	}
 
 }
